09e561517a5f570b516a9b479fcf62d5f751e708
md Asad <Asad.Md@cdk.com>
2024-09-14 17:33:12 +0530

    Our first push.

---
+++ src/ex_13092024.java
@@ -0,0 +1,9 @@
+public class ex_13092024 {
+    public static void main(String[] args)
+    {
+
+        //Constants
+        final float PIE = 3.1456f;
+        System.out.println(PIE);
+    }
+}
---
+++ src/ex_13092024/Lab020.java
@@ -0,0 +1,12 @@
+package ex_13092024;
+
+public class Lab020 {
+
+    public static void main(String[] args)
+    {
+        //WAP - print the table of 2
+        int num = 2;
+
+        System.out.printf("value of %d", num);
+    }
+}
