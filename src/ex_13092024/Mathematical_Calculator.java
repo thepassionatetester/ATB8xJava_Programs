a47b6a28f9c58d9d01386220527907361f8571d9
md Asad <Asad.Md@cdk.com>
2024-09-15 20:15:41 +0530

    Simple mathematical calculator.

---
+++ src/ex_13092024/Mathematical_Calculator.java
@@ -0,0 +1,25 @@
+package ex_13092024;
+
+public class Mathematical_Calculator {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 20;
+
+        double addition = a + b;
+        double subtraction = b-a;
+        double multiplication = a*b;
+        double division = b / a;
+        double modulus = b % a;
+
+        System.out.println("Program for simple mathematical calculator.");
+        System.out.printf("Addition result %f", addition);
+        System. out.println(' ');
+        System.out.printf("Subtraction result %f", subtraction);
+        System. out.println(' ');
+        System.out.printf("Multiplication result %f", multiplication);
+        System. out.println(' ');
+        System.out.printf("Division result %f", division);
+        System. out.println(' ');
+        System.out.printf("Modulus  result %f", modulus);
+    }
+}
